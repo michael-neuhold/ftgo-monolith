07e1b2639679bd2cc2d19980ed859ea5dcc13583
Michael Neuhold <michael@neuhold.dev>
2023-02-11 21:48:06 +0100

    added configuration

--- ftgo-courier-service/src/main/java/net/chrisrichardson/ftgo/courierservice/CourierServiceConfiguration.java
+++ ftgo-courier-service/src/main/java/net/chrisrichardson/ftgo/courierservice/CourierServiceConfiguration.java
@@ -1,4 +1,17 @@
 package net.chrisrichardson.ftgo.courierservice;
 
+import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
+import net.chrisrichardson.ftgo.courierservice.internal.web.CourierWebConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EntityScan
+@Import({CourierWebConfiguration.class, CommonSwaggerConfiguration.class})
 public class CourierServiceConfiguration {
 }
